1392d6465256f380021d5453772110d9fcc845e0
mohsahil-dot <aksty236@gmail.com>
2023-08-05 22:04:15 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import { app } from "./app.js";
+import Razorpay from "razorpay";
+import { connectDB } from "./config/database.js";
+const PORT = process.env.PORT || 4000
+
+connectDB();
+
+export const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_API_SECRET,
+  });
+
+app.listen(PORT, () => console.log(`Server is working on ${PORT}`));
